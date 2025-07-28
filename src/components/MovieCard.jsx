859f43b206514ee057183adb9cd2152ae47e6eab
gimenados <gimenasande1@gmail.com>
2025-07-27 21:43:42 -0300

    detalles cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ export default function MovieCard({ movie, onShowModal }) {
     <ul>
       <li>
         <img src={movie.imgSrc} alt={movie.title} />
-        <h2>{movie.title}</h2>
+        {/* <h2>{movie.title}</h2> */}
         <button className="btn-modern" onClick={() => onShowModal(movie)}>
           Resumen
         </button>
